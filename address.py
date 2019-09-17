77e72ef9ee30672e289086035d7a16f17d600718
wei-hub <55046786+wei-hub@users.noreply.github.com>
2019-09-17 21:15:51 +0800

    Add files via upload
    最后一次上传了。日出也看过了，就这样吧

--- address.py
+++ address.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import re
 import json
 
-#add = '1!小陈,广东省东莞市凤岗13965231525镇凤平路13号.'
+#add = '1!小陈,新疆维吾尔自治区阿克苏凤岗13965231525镇凤平路13号.'
 #add = '2!李四,福建省福州13756899511市鼓楼区鼓西街道湖滨路110号湖滨大厦一层.'
 #add = '1!张三,福建福州闽13599622362侯县上街镇福州大学10#111.'
-#add = '2!王五,福建省福州市鼓楼区五一北路18960221533123号福州鼓楼医院.'
+#add = '2!王五,福建省福州市鼓楼县五一北路18960221533123号福州鼓楼县医院.'
 #add = '3!小美,北京市东15822153326城区交道口东大街1号北京市东城区人民法院.'
-
+#add = '1!娄伤囚,浙江省杭州市丰泽区所前镇袄庄陈村13592755594工业园区5号楼.'
 add = input() #输入数据
 name = re.search('!(.*?),',add)
 name = re.sub(r'\!|\,','',name.group())#把名字提取出来
@@ -27,18 +27,25 @@ if((add1[0:2]+'市') in list):
     else :  add1 = re.sub(r'^.{2}','',add1)
     
     #判断区级城市
-    if('区' in add1):#无区字缺失
-        area = re.match('.{2}区',add1)
+    if('区' in add1 or '县' in add1):#无区字缺失
+        area = re.match(r'.*?区|县',add1)
         ans.append(area.group())
-        add1 = re.sub('.{2}区','',add1)
+        add1 = re.sub(area.group(),'',add1,1)
         #print(ans)
         #print(add1)
     else:
-        area = add1[0:2]+'区'
-        #print(city)
-        ans.append(area)
-        add1=re.sub('(^.{2})','',add1)
-        #print(add1)
+        if('区' in add1):
+            area = add1[0:2]+'区'
+            #print(city)
+            ans.append(area)
+            add1=re.sub('(^.{2})','',add1,1)
+            #print(add1)
+        elif('县' in add1):
+            area = add1[0:2]+'县'
+            #print(city)
+            ans.append(area)
+            add1=re.sub('(^.{2})','',add1,1)
+            #print(add1)
 
     #判断街道/镇/乡
     if('街道' in add1 or '镇' in add1 or '乡' in add1 or  '口' in add1):
@@ -48,46 +55,53 @@ if((add1[0:2]+'市') in list):
             #print(ans)
             #print(add1)
         else:
-            town = re.match('.*[镇|乡|街道]',add1)
+            town = re.match('.*?[镇|乡|街道]',add1)
             ans.append(town.group())
             #print(ans)
-            add1 = re.sub('.*[镇|乡|街道]','',add1)
+            add1 = re.sub(town.group(),'',add1,1)
             #print(add1)
 
     else:
         ans.append('')
         #print(ans)
 
-    #判断路/号/详细地址
-    if('街道' in add1 or '镇' in add1 or '乡' in add1 or '路' in add1 or '巷' in add1 or '街' in add1):
-        load = re.match('.*[街道|路|巷|街]',add1)
-        ans.append(load.group())
+    flag = add[0]
+    if(flag == '1'):
+        ans.append(add1)
         #print(ans)
-        add1 = re.sub('.*[街道|路|巷|街]','',add1)
-        #print(add1)
-    else:
-        ans.append('')
-
-    if( '号' in add1):
-        num = re.match('.*[号]',add1)
-        ans.append(num.group())
-        #print(ans)
-        add1 = re.sub('.*[号]','',add1)
-        #print(add1)
-
-    else:
-        ans.append('')
     
-    ans.append(add1)
-    #print(ans)
+    elif(flag == '2' or flag == '3'):
+        if('道' in add1 or '镇' in add1 or '乡' in add1 or '路' in add1 or '巷' in add1 or '街' in add1):
+            load = re.match('.*?[道|路|巷|街]',add1)
+            ans.append(load.group())
+            #print(ans)
+            add1 = re.sub(load.group(),'',add1,1)
+            #print(add1)
+        else:
+            ans.append('')
+
+        if( '号' in add1):
+            num = re.match('.*?[号]',add1)
+            ans.append(num.group())
+            #print(ans)
+            add1 = re.sub(num.group(),'',add1,1)
+            #print(add1)
+
+        else:
+            ans.append('')
+        ans.append(add1)
 
 #非直辖市
 else:
-    if('省' in add1):#无省字缺失
+    if('自治区' in add1):
+        zizi = re.match(r'.*自治区',add1)
+        ans.append(zizi.group())
+        add1 = re.sub(zizi.group(),'',add1,1)
+    elif('省' in add1):#无省字缺失
         province = re.match(r'.*省',add1)
         #print(province)
         ans.append(province.group())
-        add1 = re.sub('.*省','',add1)
+        add1 = re.sub(province.group(),'',add1,1)
         #print(ans)
         #print(add1)
     else:
@@ -106,35 +120,42 @@ else:
     
     #判断市级城市
     if('市' in add1):#无市字缺失
-        city = re.match(r'.*市',add1)
+        city = re.match(r'.*?市',add1)
         ans.append(city.group())
-        add1 = re.sub('.*市','',add1)
+        add1 = re.sub(city.group(),'',add1,1)
         #print(ans)
         #print(add1)
     else:
-        city = add1[0:2]+'市'
-        #print(city)
-        ans.append(city)
-        add1=re.sub('(^.{2})','',add1)
-        #print(add1)
+        if(add1[0:3] in ['马鞍山','六盘水','秦皇岛','石家庄','驻马店' ,'三门峡','哈尔滨' ,'牡丹江','佳木斯','七台河','双鸭山','梅河口','连云港','张家港','井冈山','景德镇','海拉尔','石嘴山','青铜峡','攀枝花','日喀则','吐鲁番','石河子','库尔勒','阿克苏']):
+            city = add1[0:3]+'市'
+            #print(city)
+            ans.append(city)
+            add1=re.sub('(^.{3})','',add1)
+            #print(add1)
+        else :
+            city = add1[0:2]+'市'
+            #print(city)
+            ans.append(city)
+            add1=re.sub('(^.{2})','',add1,1)
+            #print(add1)
 
     #判断区/县/县级市
     if('区' in add1 or '县' in add1 or '市' in add1):#无缺失
-        country = re.match(r'.*[区|县|市]',add1)
+        country = re.match(r'.*?[区|县|市]',add1)
         ans.append(country.group())
         #print(ans)
-        add1 = re.sub('.*[区|县|市]','',add1)
+        add1 = re.sub(country.group(),'',add1,1)
         #print(add1)
     else:
         ans.append('')
         #print(ans)
 
     #判断街道/镇/乡
-    if('街道' in add1 or '镇' in add1 or '乡' in add1 or '巷' in add1 or '街' in add1):
-        town = re.match('.*[街道|镇|乡]',add1)
+    if('街道' in add1 or '镇' in add1 or '乡' in add1 or '巷' in add1 or '道' in add1):
+        town = re.match('.*[街道|镇|乡|道]',add1)
         ans.append(town.group())
         #print(ans)
-        add1 = re.sub('.*[街道|镇|乡]','',add1)
+        add1 = re.sub(town.group(),'',add1,1)
         #print(add1)
 
     else:
@@ -149,19 +170,19 @@ else:
     
     elif(flag == '2' or flag == '3'):
         if('街道' in add1 or '镇' in add1 or '乡' in add1 or '路' in add1 or '巷' in add1 or '街' in add1):
-            load = re.match('.*[街道|路|巷|街]',add1)
+            load = re.match('.*?[街道|路|巷|街]',add1)
             ans.append(load.group())
             #print(ans)
-            add1 = re.sub('.*[街道|路|巷|街]','',add1)
+            add1 = re.sub(load.group(),'',add1,1)
             #print(add1)
         else:
             ans.append('')
 
         if( '号' in add1):
-            num = re.match('.*[号]',add1)
+            num = re.match('.*?[号]',add1)
             ans.append(num.group())
             #print(ans)
-            add1 = re.sub('.*[号]','',add1)
+            add1 = re.sub(num.group(),'',add1,1)
             #print(add1)
 
         else:
@@ -171,13 +192,3 @@ else:
 dict['地址'] = ans
 #print(dict)
 print(json.dumps(dict))
-
-
-
-
-
-
-
-
-
-
